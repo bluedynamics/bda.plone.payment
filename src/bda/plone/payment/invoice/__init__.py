571b3b5c25e3ec3286863361326a6930f6482033
Robert Niederreiter <office@squarewave.at>
2013-03-07 17:01:52 +0100

    housekeeping

--- src/bda/plone/payment/invoice/__init__.py
+++ src/bda/plone/payment/invoice/__init__.py
@@ -14,22 +14,22 @@ class Invoice(Payment):
     label = _('invoice', 'Invoice')
     available = True
     default = False
-    
+
     def init_url(self, uid):
         return '%s/@@invoice?uid=%s' % (self.context.absolute_url(), uid)
 
 
 class DoInvoice(BrowserView):
-        
+
     def __call__(self, **kw):
         uid = self.request['uid']
         payment = Payments(self.context).get('invoice')
-        payment.succeed(self.request, self.request['uid'])
-        
-        self.request.response.redirect('%s/@@invoiced?uid=%s' % (self.context.absolute_url(), uid))
-                
+        payment.succeed(self.request, uid)
+        url = '%s/@@invoiced?uid=%s' % (self.context.absolute_url(), uid)
+        self.request.response.redirect(url)
+
+
 class InvoiceFinished(BrowserView):
-    
+
     def id(self):
         return self.request.get('uid', None)
-        
