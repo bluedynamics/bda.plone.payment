291dcbe8192c93e3fd070119f14f8e0af86268e2
Robert Niederreiter <office@squarewave.at>
2013-07-19 16:14:52 +0200

    cleanup whitespace

--- src/bda/plone/payment/interfaces.py
+++ src/bda/plone/payment/interfaces.py
@@ -10,23 +10,23 @@ class IPaymentExtensionLayer(Interface):
 
 
 class IPaymentEvent(Interface):
-    """Payment related event
+    """Payment related event.
     """
     context = Attribute(u"Context in which this event was triggered.")
-    
+
     request = Attribute(u"Current request.")
-    
+
     payment = Attribute(u"Payment instance.")
-    
+
     order_uid = Attribute(u"Referring order uid.")
-    
+
     data = Attribute(u"Optional data as dict")
 
 
 class IPaymentSuccessEvent(IPaymentEvent):
     """This event gets triggered when payment was successful.
     """
-    
+
 
 class IPaymentFailedEvent(IPaymentEvent):
     """This event gets triggered when payment failed.
@@ -38,22 +38,22 @@ class IPayment(Interface):
     """
     pid = Attribute(u"Unique payment id. Payment adapter is also registered "
                     u"under this name.")
-    
+
     label = Attribute(u"Payment label")
-    
+
     available = Attribute(u"Flag whether payment is available in recent "
                           u"payment cycle")
-    
+
     default = Attribute(u"Flag whether this payment is default payment.")
-    
+
     def init_url(uid):
         """Return payment initialization URL.
         """
-    
+
     def succeed(request, order_uid, data=dict()):
         """Notify ``IPaymentSuccessEvent``.
         """
-    
+
     def failed(request, order_uid, data=dict()):
         """Notify ``IPaymentFailedEvent``.
         """
@@ -62,7 +62,7 @@ class IPayment(Interface):
 class IPaymentData(Interface):
     """Interface for providing data required by payment.
     """
-    
+
     def data(order_uid):
         """Return payment related data.
         """
