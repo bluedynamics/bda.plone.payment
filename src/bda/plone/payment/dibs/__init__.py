4623e0f9397e1880ab6b1a2ecf32df526da49641
Espen Moe-Nilssen <espen@medialog.no>
2013-08-05 11:44:46 +0200

    Removed hard coding of language

--- src/bda/plone/payment/dibs/__init__.py
+++ src/bda/plone/payment/dibs/__init__.py
@@ -40,16 +40,18 @@ class DoDibs(BrowserView):
     """
     def __call__(self, **kw):
         order_uid = self.request['uid']
+        uid = self.request['uid']
+        
         #registry = getUtility(IRegistry)
         #settings = registry.forInterface(IShopSettings)
 
         #get language
         context = aq_inner(self.context)
         portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
-        #current_language = portal_state.language()
-        current_language = "nb_NO"
+        current_language = portal_state.language()
+        #current_language = "nb_NO"
+        
         dibs_url = CREATE_PAY_INIT_URL
-        uid = self.request['uid']
         
         data = ISixPaymentData(self.context).data(order_uid)        
         payment = Payments(self.context).get('dibs')
