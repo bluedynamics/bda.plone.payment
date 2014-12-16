9593dfaeddabd3c81f8fa1aba76c2286e9b5daf1
Peter Holzer <hpeter@agitator.com>
2014-12-16 22:02:38 +0100

    "bda.recipe.deployment pre tag commit"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '0.6.dev1'
+version = '0.6.dev1.sfama'
 shortdesc = "Payment"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
