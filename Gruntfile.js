5bd3e53066c8a35314d8206f925f4aa2252d4da3
Robert Niederreiter <office@squarewave.at>
2016-04-06 16:20:13 +0200

    straight forward resource registration

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,6 @@ module.exports = function (grunt) {
     'use strict';
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        // we could just concatenate everything, really
-        // but we like to have it the complex way.
-        // also, in this way we do not have to worry
-        // about putting files in the correct order
-        // (the dependency tree is walked by r.js)
         less: {
             dist: {
                 options: {
@@ -14,57 +9,35 @@ module.exports = function (grunt) {
                     strictMath: false,
                     sourceMap: true,
                     outputSourceFiles: true,
-                    sourceMapURL: '++resource++bda.plone.payment-compiled.css.map',
-                    sourceMapFilename: 'src/bda/plone/payment/resources/payment-compiled.css.map',
+                    sourceMapURL: '++resource++bda.plone.payment.css.map',
+                    sourceMapFilename: 'src/bda/plone/payment/resources/payment_p5.css.map',
                     modifyVars: {
-                        isPlone: "false"
+                        "isPlone": "false"
                     }
                 },
                 files: {
-                    'src/bda/plone/payment/resources/payment-compiled.css': 'src/bda/plone/payment/resources/payment.less'
+                    'src/bda/plone/payment/resources/payment_p5.css': 'src/bda/plone/payment/resources/payment_p5.less',
                 }
             }
         },
-
+        sed: {
+            sed0: {
+                path: 'src/bda/plone/payment/resources/payment_p5.css.map',
+                pattern: 'src/bda/plone/payment/resources/payment_p5.less',
+                replacement: '++resource++bda.plone.payment.less',
+            }
+        },
         watch: {
             scripts: {
-                files: ['src/bda/plone/payment/resources/*.less'],
-                tasks: ['less']
-            }
-        },
-        browserSync: {
-            html: {
-                bsFiles: {
-                    src: ['src/bda/plone/payment/resources/*.less']
-                },
-                options: {
-                    watchTask: true,
-                    debugInfo: true,
-                    server: {
-                        baseDir: "."
-                    }
-                }
-            },
-            plone: {
-                bsFiles: {
-                    src: ['src/bda/plone/payment/resources/*.less']
-                },
-                options: {
-                    watchTask: true,
-                    debugInfo: true,
-                    proxy: "localhost:8080"
-                }
+                files: ['src/bda/plone/payment/resources/payment_p5.less'],
+                tasks: ['less', 'sed']
             }
         }
     });
-
-    // grunt.loadTasks('tasks');
-    grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-sed');
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('bsync', ["browserSync:html", "watch"]);
-    grunt.registerTask('plone-bsync', ["browserSync:plone", "watch"]);
+    grunt.registerTask('compile', ['less', 'sed']);
 };
