4515009ebab1dc3b358876b192da854e49be6d94
Peter Mathis <peter.mathis@kombinat.at>
2024-04-17 11:52:26 +0200

    add uninstall profile

--- src/bda/plone/payment/setuphandlers.py
+++ src/bda/plone/payment/setuphandlers.py
@@ -1,19 +1,15 @@
 # -*- coding:utf-8 -*-
-from Products.CMFPlone import interfaces as Plone
-from Products.CMFQuickInstallerTool import interfaces as QuickInstaller
+from plone.base.interfaces import INonInstallable
 from zope.interface import implementer
 
 
-@implementer(Plone.INonInstallable)
+@implementer(INonInstallable)
 class HiddenProfiles(object):
     def getNonInstallableProfiles(self):
         """Do not show on Plone's list of installable profiles.
         """
         return []
 
-
-@implementer(QuickInstaller.INonInstallable)
-class HiddenProducts(object):
     def getNonInstallableProducts(self):
         """Do not show on QuickInstaller's list of installable products.
         """
